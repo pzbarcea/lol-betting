1c7eac0e5aca61300551dcbff95ddd5dd47cbcef
Paul Zbarcea <paul.zbarcea@gmail.com>
2022-06-23 03:33:42 +0300

    tutorial pt2 done - working hello and goodbye with hardcoded bearer token

---
+++ src/utils/server.ts
@@ -0,0 +1,66 @@
+import express from 'express'
+// import {OpenApiValidator} from 'express-openapi-validator' // if version 3.*
+import * as OpenApiValidator from 'express-openapi-validator'
+import {Express} from 'express-serve-static-core'
+import {connector, summarise} from 'swagger-routes-express'
+import YAML from 'yamljs'
+ 
+import * as api from '../api/controllers'
+ 
+export async function createServer(): Promise<Express> {
+  const yamlSpecFile = './config/openapi.yml'
+  const apiDefinition = YAML.load(yamlSpecFile)
+  const apiSummary = summarise(apiDefinition)
+  console.info(apiSummary)
+ 
+  const server = express()
+  // here we can intialize body/cookies parsers, connect logger, for example morgan
+ 
+  // setup API validator
+  const validatorOptions = {
+    coerceTypes: true,
+    apiSpec: yamlSpecFile,
+    validateRequests: true,
+    validateResponses: true
+  }
+//   await new OpenApiValidator(validatorOptions).install(server) // if version 3.*
+  server.use(OpenApiValidator.middleware(validatorOptions))
+  
+  // error customization, if request is invalid
+  server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(err.status).json({
+      error: {
+        type: 'request_validation',
+        message: err.message,
+        errors: err.errors
+      }
+    })
+  })
+ 
+  const connect = connector(api, apiDefinition, {
+    onCreateRoute: (method: string, descriptor: any[]) => {
+      descriptor.shift()
+      console.log(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`)
+    },
+    security: {
+      bearerAuth: api.auth
+    }
+  })
+
+  connect(server)
+ 
+  return server
+}
+
+
+
+// import express from 'express'
+// import {Express} from 'express-serve-static-core'
+
+// export async function createServer(): Promise<Express> {
+//   const server = express()
+//   server.get('/', (req, res) => {
+//     res.send('Hello world!!!')
+//   })
+//   return server
+// }
